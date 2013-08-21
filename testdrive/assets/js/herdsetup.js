eae1f8200c7eacf871a7541e2e8054fd03501a7c
Jai <jaisankar.n@gmail.com>
2013-08-21 22:51:49 +0530

    Project D  changes Validations

--- testdrive/assets/js/herdsetup.js
+++ testdrive/assets/js/herdsetup.js
@@ -14,3 +14,17 @@ $( "<div>" )
 }
 });
 });
+
+function checkData(element,type){
+	if(type == 1){
+		var val = element.value;
+		var patt = /[0-9][A-Z]/
+		if(!patt.test(val)){
+			alert("Farm Herd - Not in valid format");
+			$("#herd").val("");
+		}
+		$("#herd").focus();
+		
+	}
+	
+}
--- testdrive/protected/views/tblHerdSetup/_form.php
+++ testdrive/protected/views/tblHerdSetup/_form.php
@@ -16,14 +16,14 @@
 	<table>
 <tr><td width="50%"><div class="row">
 		<?php echo $form->labelEx($model,'farm_herd'); ?>
-		<?php echo $form->textField($model,'farm_herd',array('size'=>2,'maxlength'=>2)); ?>
+		<?php echo $form->textField($model,'farm_herd',array('size'=>2,'id'=>'herd','maxlength'=>2,'onkeyup'=>'caps(this)',onBlur=>'checkData(this,1)')); ?>
 		<?php echo $form->error($model,'farm_herd'); ?>
 		</div>
 	</td>
 	<td width="50%">&nbsp;</td></tr>
 <tr><td colspan="2"><div class="row">
 		<?php echo $form->labelEx($model,'breeder_name'); ?>
-		<?php echo $form->textField($model,'breeder_name',array('size'=>50,'maxlength'=>50)); ?>
+		<?php echo $form->textField($model,'breeder_name',array('size'=>50,'maxlength'=>50,'id'=>'herd1')); ?>
 		<?php echo $form->error($model,'breeder_name'); ?>
 	</div></td>
 	</tr>
@@ -34,7 +34,7 @@
 	</div></td></tr>
 <tr><td colspan="2"><div class="row">
 		<?php echo $form->labelEx($model,'address1'); ?>
-		<?php echo $form->textField($model,'address1',array('size'=>60,'maxlength'=>100)); ?>
+		<?php echo $form-> textField($model,'address1',array('size'=>60,'maxlength'=>100)); ?>
 		<?php echo $form->error($model,'address1'); ?>
 	</div></td>
 	</tr>
@@ -77,7 +77,7 @@
 		<?php echo $form->error($model,'breeder_number'); ?>
 	</div></td><td><div class="row">
 		<?php echo $form->labelEx($model,'take_sow_scores'); ?>
-		<?php echo $form->textField($model,'take_sow_scores',array('size'=>0,'maxlength'=>0)); ?>
+		<?php echo $form->dropDownList($model,'take_sow_scores',array('Y'=>'Y','N'=>'N'),array('size'=>0,'maxlength'=>0,'title'=>'Y=Take Sow disposition, Underline, Front Leg, Rear Leg Scores N=Not Take Scores')); ?>
 		<?php echo $form->error($model,'take_sow_scores'); ?>
 	</div></td>
 </tr>
@@ -88,18 +88,18 @@
 	</div></td>
 	<td><div class="row">
 		<?php echo $form->labelEx($model,'spring_start'); ?>
-		<?php echo $form->textField($model,'spring_start'); ?>
+		<?php echo $form->textField($model,'spring_start',array('title'=>'Beginning Month for spring farrowing, F4=The Way It used to be')); ?>
 		<?php echo $form->error($model,'spring_start'); ?>
 	</div></td>
 	</tr>
 <tr><td><div class="row">
 		<?php echo $form->labelEx($model,'home_country'); ?>
-		<?php echo $form->textField($model,'home_country',array('size'=>1,'maxlength'=>1,'title'=>'C-Canada, M-Mexico, U-USA')); ?>
+		<?php echo $form->dropDownList($model,'home_country',array('C'=>'C','M'=>'M','U'=>'U'),array('size'=>1,'maxlength'=>1,'title'=>'C-Canada, M-Mexico, U-USA')); ?>
 		<?php echo $form->error($model,'home_country'); ?>
 	</div></td>
 	<td><div class="row">
 		<?php echo $form->labelEx($model,'spring_end'); ?>
-		<?php echo $form->textField($model,'spring_end'); ?>
+		<?php echo $form->textField($model,'spring_end',array('title'=>'Ending Month for spring farrowing, F4=The Way It used to be')); ?>
 		<?php echo $form->error($model,'spring_end'); ?>
 	</div></td>
 	</tr>
@@ -110,62 +110,62 @@
 	</div></td>
 	<td><div class="row">
 		<?php echo $form->labelEx($model,'spring_letter'); ?>
-		<?php echo $form->textField($model,'spring_letter',array('size'=>1,'maxlength'=>1)); ?>
+		<?php echo $form->textField($model,'spring_letter',array('size'=>1,'maxlength'=>1,'title'=>'S Or Blank for spring farrowing, F4=The Way It used to be')); ?>
 		<?php echo $form->error($model,'spring_letter'); ?>
 	</div></td>
 	</tr>
 <tr><td><div class="row">
 		<?php echo $form->labelEx($model,'is_weight'); ?>
-		<?php echo $form->textField($model,'is_weight',array('size'=>1,'maxlength'=>1,'title'=>'Y-Take Litter Birth Weight, N-Not Take Birth Weight')); ?>
+		<?php echo $form->dropDownList($model,'is_weight',array('Y'=>'Y','N'=>'N'),array('size'=>1,'maxlength'=>1,'title'=>'Y-Take Litter Birth Weight, N-Not Take Birth Weight')); ?>
 		<?php echo $form->error($model,'is_weight'); ?>
 	</div></td>
 	<td><div class="row">
 		<?php echo $form->labelEx($model,'fall_start'); ?>
-		<?php echo $form->textField($model,'fall_start'); ?>
+		<?php echo $form->textField($model,'fall_start',array('title'=>'Beginning Month for Fall farrowing, F4=The Way It used to be')); ?>
 		<?php echo $form->error($model,'fall_start'); ?>
 	</div></td>
 	</tr>
 <tr><td><div class="row">
 		<?php echo $form->labelEx($model,'breeder_no'); ?>
-		<?php echo $form->textField($model,'breeder_no',array('size'=>1,'maxlength'=>1)); ?>
+		<?php echo $form->dropDownList($model,'breeder_no',array('N'=>'N','#'=>'#'),array('size'=>1,'maxlength'=>1,'title'=>'N=BOAR names will be printed on reports #=BOAR number will be used')); ?>
 		<?php echo $form->error($model,'breeder_no'); ?>
 	</div></td>
 	<td><div class="row">
 		<?php echo $form->labelEx($model,'fall_end'); ?>
-		<?php echo $form->textField($model,'fall_end'); ?>
+		<?php echo $form->textField($model,'fall_end',array('title'=>'Ending Month for fall farrowing, F4=The Way It used to be')); ?>
 		<?php echo $form->error($model,'fall_end'); ?>
 	</div></td>
 	</tr>
 <tr><td><div class="row">
 		<?php echo $form->labelEx($model,'weighted'); ?>
-		<?php echo $form->textField($model,'weighted',array('size'=>0,'maxlength'=>0)); ?>
+		<?php echo $form->dropDownList($model,'weighted',array('N'=>'N','#'=>'#'),array('size'=>0,'maxlength'=>0,'title'=>'N=Litter Weighed Age entry #=Litter Weighed date entry')); ?>
 		<?php echo $form->error($model,'weighted'); ?>
 	</div></td>
 	<td><div class="row">
 		<?php echo $form->labelEx($model,'fall_letter'); ?>
-		<?php echo $form->textField($model,'fall_letter',array('size'=>1,'maxlength'=>1)); ?>
+		<?php echo $form->textField($model,'fall_letter',array('size'=>1,'maxlength'=>1,'title'=>'F Or Blank for fall farrowing, F4=The Way It used to be')); ?>
 		<?php echo $form->error($model,'fall_letter'); ?>
 	</div></td>
 	</tr>
 <tr><td><div class="row">
 		<?php echo $form->labelEx($model,'is_hog_tag'); ?>
-		<?php echo $form->textField($model,'is_hog_tag',array('size'=>0,'maxlength'=>0)); ?>
+		<?php echo $form->dropDownList($model,'is_hog_tag',array('H'=>'H','T'=>'T'),array('size'=>0,'maxlength'=>0,'title'=>'H=Hog Ear Notch Entry T=Hog Ear Tag Entry')); ?>
 		<?php echo $form->error($model,'is_hog_tag'); ?>
 	</div></td>
 	<td><div class="row">
 		<?php echo $form->labelEx($model,'shift_year'); ?>
-		<?php echo $form->textField($model,'shift_year',array('size'=>1,'maxlength'=>1)); ?>
+		<?php echo $form->textField($model,'shift_year',array('size'=>1,'maxlength'=>1,'title'=>'Shift Year if season jumps year boundary, L-Leave Year as is, F4=The old way')); ?>
 		<?php echo $form->error($model,'shift_year'); ?>
 	</div></td>
 	</tr>
 <tr><td><div class="row">
 		<?php echo $form->labelEx($model,'hog_numbering'); ?>
-		<?php echo $form->textField($model,'hog_numbering',array('size'=>0,'maxlength'=>0)); ?>
+		<?php echo $form->dropDownList($model,'hog_numbering',array('Q'=>'Q','N'=>'N'),array('size'=>0,'maxlength'=>0,'title'=>'Q=Quebec Canada Hog Numbers N=Non Specialized Hog Numbers')); ?>
 		<?php echo $form->error($model,'hog_numbering'); ?>
 	</div></td>
 	<td><div class="row">
 		<?php echo $form->labelEx($model,'take_weaned_date'); ?>
-		<?php echo $form->textField($model,'take_weaned_date',array('size'=>0,'maxlength'=>0)); ?>
+		<?php echo $form->textField($model,'take_weaned_date',array('size'=>0,'maxlength'=>0,'title'=>'T=Take Weaned Date In letters weaned & weighed, S-Set weaned date to weighed date')); ?>
 		<?php echo $form->error($model,'take_weaned_date'); ?>
 	</div></td>
 	</tr>
@@ -176,7 +176,7 @@
 	</div></td>
 	<td><div class="row">
 		<?php echo $form->labelEx($model,'take_deffects'); ?>
-		<?php echo $form->textField($model,'take_deffects',array('size'=>0,'maxlength'=>0)); ?>
+		<?php echo $form->textField($model,'take_deffects',array('size'=>0,'maxlength'=>0,'title'=>'Y-Take Defects, N-Not take Defects')); ?>
 		<?php echo $form->error($model,'take_deffects'); ?>
 	</div></td>
 	</tr>
@@ -187,13 +187,13 @@
 	</div></td>
 	<td><div class="row">
 		<?php echo $form->labelEx($model,'prev_herd_mark'); ?>
-		<?php echo $form->textField($model,'prev_herd_mark',array('size'=>10,'maxlength'=>10)); ?>
+		<?php echo $form->textField($model,'prev_herd_mark',array('size'=>10,'maxlength'=>10,'title'=>'Only use when you have changed this herd\'s Herd mark')); ?>
 		<?php echo $form->error($model,'prev_herd_mark'); ?>
 	</div></td>
 </tr>
 <tr><td><div class="row">
 		<?php echo $form->labelEx($model,'take_boars_gilts'); ?>
-		<?php echo $form->textField($model,'take_boars_gilts',array('size'=>0,'maxlength'=>0)); ?>
+		<?php echo $form->dropDownList($model,'take_boars_gilts',array('Y'=>'Y','N'=>'N'),array('size'=>0,'maxlength'=>0,'title'=>'Y=Take # Boars & Gilts Born at farrowing N=Not Take #\'s')); ?>
 		<?php echo $form->error($model,'take_boars_gilts'); ?>
 	</div></td><td>&nbsp;</td></tr>
 <tr><td>&nbsp;</td></tr>
