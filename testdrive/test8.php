0d3ee47d8fcd96a1ae6e44d610887c522dd3b2ea
jai <jaisankar.n@gmail.com>
2013-09-29 14:16:24 +0530

    Arrays

--- testdrive/test8.php
+++ testdrive/test8.php
@@ -5,7 +5,7 @@
 	border-top: 1px solid black; 
 	border-bottom: 1px solid black;
 	border-left: 1px solid black; 
-	height: 730px;
+	height: 780px;
 }
 .a1{
 	border-top: 1px solid black; 
@@ -40,7 +40,7 @@
 </style>
   </head>
   <body>
-    <table width="10%" style="position: absolute; top: 400px;" border="0">
+    <table width="10%" style="position: absolute; top: 390px;" border="0">
 	    <tr><td class="a" rowspan="2">Test</td></tr>
     </table>
  <?php
@@ -57,48 +57,55 @@ $level[2] = str_replace("$2","Mathangi",$level[2]);
 echo $level[2];
 ?>
 <?php 
+$level[3] = "";
 $level3 = array("130","470","920","1260");
 foreach($level3 as $key=>$levels){
-?>
-    <table width="10%" style="position: absolute; top: <?php echo $levels; ?>px; left:22%" border="0">
-	    <tr><td class="a2" rowspan="2">Test</td></tr>
-    </table>
-<?php
+ $level[3] .= '<table width="10%" style="position: absolute; top: '.$levels.'px; left:22%" border="0">
+	    <tr><td class="a2" rowspan="2">$'.($key+1).'</td></tr>
+    </table>';
 }
+$level[3] = str_replace("$1","JAISANKAR",$level[3]);
+$level[3] = str_replace("$2","Mathangi",$level[3]);
+echo $level[3];
 ?>
     
 <?php 
+$level[4] = "";
 $level4 = array("90","270","430","610","880","1060","1220","1400");
 foreach($level4 as $key=>$levels){
 	$style = "";
 	//if($key == 4)
 		//$style = "style='height: 60px;'";
-?>
-   <table width="10%" style="position: absolute; top: <?php echo $levels; ?>px; left:33%" border="0">
-	    <tr><td class="a3" rowspan="2" <?php echo $style; ?>><?php echo $key; ?>Test</td></tr>
-   </table>
-<?php
+$level[4] .=  '<table width="10%" style="position: absolute; top: '.$levels.'px; left:33%" border="0">
+	    <tr><td class="a3" rowspan="2" <?php echo $style; ?>$'.($key+1).'</td></tr>
+   </table>';
 }
+$level[4] = str_replace("$1","JAISANKAR",$level[4]);
+$level[4] = str_replace("$2","Mathangi",$level[4]);
+echo $level[4];
 ?>
 <?php 
+$level[5] = "";
 $level5 = array("70","150","250","330","410","490","590","670","860","940","1050","1120","1200","1280","1380","1460");
 foreach($level5 as $key=>$levels){
-?>
-   <table width="10%" style="position: absolute; top: <?php echo $levels; ?>px; left:44%" border="0">
-	    <tr><td class="a4" rowspan="2"><?php echo $key; ?>Test</td></tr>
-   </table>
-<?php
+ $level[5] .=  '  <table width="10%" style="position: absolute; top: '.$levels.'px; left:44%" border="0">
+	    <tr><td class="a4" rowspan="2"><?php echo $key; ?>$'.($key+1).'</td></tr>
+   </table>';
 }
-?>
-<?php 
-$level5 = array("60","100","140","180","240","280","320","360","400","435","480","520","580","620","660","695","850","890","930","960","1040","1080","1110","1150","1190","1220","1270","1300","1370","1400","1450","1480");
-foreach($level5 as $key=>$levels){
-?>
-   <table width="10%" style="position: absolute; top: <?php echo $levels; ?>px; left:55%" border="0">
-	    <tr><td class="a5" rowspan="2"><?php echo $key; ?>Test</td></tr>
-   </table>
-<?php
+$level[5] = str_replace("$1","JAISANKAR",$level[5]);
+$level[5] = str_replace("$2","Mathangi",$level[5]);
+echo $level[5];
+
+$level[6] = "";
+$level6 = array("60","100","140","180","240","280","320","360","400","435","480","520","580","620","660","695","850","890","930","960","1040","1080","1110","1150","1190","1220","1270","1300","1370","1400","1450","1480");
+foreach($level6 as $key=>$levels){
+   $level[6] .=  '  <table width="10%" style="position: absolute; top: '.$levels.'px; left:55%" border="0">
+	    <tr><td class="a5" rowspan="2"><?php echo $key; ?>$'.($key+1).'</td></tr>
+   </table>';
 }
+$level[6] = str_replace("$1","JAISANKAR",$level[6]);
+$level[6] = str_replace("$2","Mathangi",$level[6]);
+echo $level[6];
 ?>
 </body>
 </html>
