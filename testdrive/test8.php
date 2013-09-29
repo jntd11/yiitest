15cbf6b73df60c2c424c0dd3b2cc46c885e83fc4
jai <jaisankar.n@gmail.com>
2013-09-29 12:17:56 +0530

    Added Test8

--- testdrive/test8.php
+++ testdrive/test8.php
@@ -5,25 +5,25 @@
 	border-top: 1px solid black; 
 	border-bottom: 1px solid black;
 	border-left: 1px solid black; 
-	height: 500px;
+	height: 730px;
 }
 .a1{
 	border-top: 1px solid black; 
 	border-bottom: 1px solid black;
 	border-left: 1px solid black; 
-	height: 230px;
+	height: 340px;
 }
 .a2{
 	border-top: 1px solid black; 
 	border-bottom: 1px solid black;
 	border-left: 1px solid black; 
-	height: 140px;
+	height: 180px;
 }
 .a3{
 	border-top: 1px solid black; 
 	border-bottom: 1px solid black;
 	border-left: 1px solid black; 
-	height: 60px;
+	height: 80px;
 }
 .a4{
 	border-top: 1px solid black; 
@@ -31,22 +31,33 @@
 	border-left: 1px solid black; 
 	height: 40px;
 }
+.a5{
+	border-top: 1px solid red; 
+	border-bottom: 1px solid red;
+	border-left: 1px solid red; 
+	height: 9px;
+}
 </style>
   </head>
   <body>
     <table width="10%" style="position: absolute; top: 400px;" border="0">
 	    <tr><td class="a" rowspan="2">Test</td></tr>
     </table>
-    
-    <table width="10%" style="position: absolute; top: 290px; left:11%" border="0">
-	    <tr><td class="a1" rowspan="2">Test</td></tr>
+ <?php
+ $level[2] = '
+	 <table width="10%" style="position: absolute; top: 220px; left:11%" border="0">
+	    <tr><td class="a1" rowspan="2">$1</td></tr>
     </table>
-    <table width="10%" style="position: absolute; top: 790px; left:11%" border="0">
-	    <tr><td class="a1" rowspan="2">Test</td></tr>
-    </table>
-    
+    <table width="10%" style="position: absolute; top: 1010px; left:11%" border="0">
+	    <tr><td class="a1" rowspan="2">$2</td></tr>
+    </table>';
+
+$level[2] = str_replace("$1","JAISANKAR",$level[2]);
+$level[2] = str_replace("$2","Mathangi",$level[2]);
+echo $level[2];
+?>
 <?php 
-$level3 = array("220","450","720","950");
+$level3 = array("130","470","920","1260");
 foreach($level3 as $key=>$levels){
 ?>
     <table width="10%" style="position: absolute; top: <?php echo $levels; ?>px; left:22%" border="0">
@@ -57,24 +68,34 @@ foreach($level3 as $key=>$levels){
 ?>
     
 <?php 
-$level4 = array("180","320","430","550","680","830","930","1050");
+$level4 = array("90","270","430","610","890","1020","1150","1290");
 foreach($level4 as $key=>$levels){
 	$style = "";
-	//if($key == 1 or $key == 2 or $key == 7)
+	//if($key == 4)
 		//$style = "style='height: 60px;'";
 ?>
    <table width="10%" style="position: absolute; top: <?php echo $levels; ?>px; left:33%" border="0">
-	    <tr><td class="a3" rowspan="2" <?php echo $style; ?>>Test</td></tr>
+	    <tr><td class="a3" rowspan="2" <?php echo $style; ?>><?php echo $key; ?>Test</td></tr>
    </table>
 <?php
 }
 ?>
 <?php 
-$level5 = array("160","220","300","360","410","470","530","590","660","720","800","860","910","970","1030","1090");
+$level5 = array("70","150","250","330","410","490","590","670","870","950","1000","1080","1135","1210","1270","1350");
 foreach($level5 as $key=>$levels){
 ?>
    <table width="10%" style="position: absolute; top: <?php echo $levels; ?>px; left:44%" border="0">
-	    <tr><td class="a4" rowspan="2">Test</td></tr>
+	    <tr><td class="a4" rowspan="2"><?php echo $key; ?>Test</td></tr>
+   </table>
+<?php
+}
+?>
+<?php 
+$level5 = array("60","100","140","180","240","280","320","360","400","435","480","520","580","620","660","695","715","745","770","790","790","820","870","900","930","960","1000","1040","1070","1100","1140","1180");
+foreach($level5 as $key=>$levels){
+?>
+   <table width="10%" style="position: absolute; top: <?php echo $levels; ?>px; left:55%" border="0">
+	    <tr><td class="a5" rowspan="2"><?php echo $key; ?>Test</td></tr>
    </table>
 <?php
 }
