4b2b2edf3b0a6e5e2c699d83434dcffb7fe1c472
jntd11 <jaisankar.n@gmail.com>
2013-10-09 14:21:31 +0530

    Reversed old level 5

--- testdrive/protected/config/main.php
+++ testdrive/protected/config/main.php
@@ -58,7 +58,7 @@ return array(
 			'connectionString' => 'mysql:host=localhost;dbname=testdrive',
 			'emulatePrepare' => true,
 			'username' => 'root',
-			'password' => 'rootya rte',
+			'password' => '',
 			'charset' => 'utf8',
 		),
 		
